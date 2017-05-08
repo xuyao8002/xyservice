fab1f64bf26d5b9a8ffe99dfd4d38f72063a66eb
xu yao <xu yao>
2017-05-08 22:58:47 +0800

    ehcache

--- src/main/java/com/xuyao/service/BaseServiceImpl.java
+++ src/main/java/com/xuyao/service/BaseServiceImpl.java
@@ -1,9 +1,17 @@
 package com.xuyao.service;
 
+import com.xuyao.framework.ehcache.dao.EhCacheDao;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Collection;
+
 /**
  * Created by xuyao on 2017/4/16.
  */
 public class BaseServiceImpl implements BaseService {
+
+    @Autowired
+    private EhCacheDao ehCacheDao;
 
     @Override
     public void printInfo() {
@@ -20,5 +28,30 @@ public class BaseServiceImpl implements BaseService {
 		
 		return num + ", Hello Kitty";
 	}
-    
+
+    @Override
+    public void put(String s, Object o, int i, int i1) {
+        ehCacheDao.put(s, o, i, i1);
+    }
+
+    @Override
+    public <T> T get(String s) {
+        return ehCacheDao.get(s);
+    }
+
+    @Override
+    public boolean remove(String s) {
+        return ehCacheDao.remove(s);
+    }
+
+    @Override
+    public void removeAll(Collection<?> collection) {
+        ehCacheDao.removeAll(collection);
+    }
+
+    @Override
+    public void removeAll() {
+        ehCacheDao.removeAll();
+    }
+
 }
